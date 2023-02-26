41355d8d3d76f30196ad38f5da5ae3d587bd6cf5
alfredo cruz <arandevs22@gmail.com>
2023-02-26 01:56:06 -0600

    navbar commit

--- src/pages/Busqueda.jsx
+++ src/pages/Busqueda.jsx
@@ -1,25 +1,23 @@
-import { AppBar, Box, Container, Link, Toolbar, Typography } from "@mui/material"
+import { AppBar, Box, Container, createTheme, Link, Toolbar, Typography } from "@mui/material"
 import SearchIcon from '@mui/icons-material/Search';
+
+
 
 export const Busqueda = () => {
     return (
         <>
-            <Box sx={{ flexGrow: 1, top: 0 }}>
-                <AppBar position='fixed'>
-                    <Toolbar>
-                        <Typography variant='h5' component='div' sx={{ flexGrow: 1 }}>
-                            Buscar
-                        </Typography>
-                        <Link href="http://action_search" color='inherit'>
-                            <SearchIcon sx={{ fontSize: 30 }} />
-                        </Link>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-            <Container>
-
-            </Container>
-
+            <AppBar position='sticky'>
+                <Toolbar>
+                    <Typography variant='h5' component='div' sx={{ flexGrow: 1 }}>
+                        Buscar
+                    </Typography>
+                    <Link href="http://action_search" color='inherit'>
+                        <SearchIcon sx={{ fontSize: 30 }} />
+                    </Link>
+                </Toolbar>
+            </AppBar>
+            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque repellendus ipsa sint pariatur nihil at asperiores iste ullam officia, quis qui assumenda exercitationem sit architecto error blanditiis omnis dolores quia.</h1>
+            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque repellendus ipsa sint pariatur nihil at asperiores iste ullam officia, quis qui assumenda exercitationem sit architecto error blanditiis omnis dolores quia.</h1>
         </>
     )
 }
