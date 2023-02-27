d6b95671ec358fbb409ec40fa052f292c2a84bb4
alfredo cruz <arandevs22@gmail.com>
2023-02-26 21:11:08 -0600

    shooter games commit

--- src/pages/generos/Shooter.jsx
+++ src/pages/generos/Shooter.jsx
@@ -1,7 +1,30 @@
+import { AppBar, Container, styled, Toolbar, Typography, Grid, Box, ImageList, ImageListItem } from "@mui/material"
+import { shooterData } from "../../data/games";
+
+const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
+
 export const Shooter = () => {
+
     return (
         <>
-            <h1>Shooter</h1>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Typography variant="h5" component='div' sx={{ flexGrow: 1 }}>
+                        Shooter
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Offset />
+            <Container>
+                <ImageList cols={3}>
+                    {shooterData.map((game) => (
+                        <ImageListItem key={game.poster}>
+                            <img src={game.poster} alt={game.title} className='cover' loading='lazy' />
+                        </ImageListItem>
+                    ))}
+                </ImageList>
+            </Container>
         </>
     )
 }
+
